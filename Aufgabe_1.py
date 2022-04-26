444bbf62326921ad041ff4bc9559c0b8529a94a8
Amex221 <amel.abdic1999@gmail.com>
2022-04-26 20:03:16 +0200

    Aufgabe1

--- Aufgabe_1.py
+++ Aufgabe_1.py
@@ -1,7 +1,60 @@
-def main():
+from array import array
+from datetime import date
+from multiprocessing.dummy import Array
+from datetime import datetime
+
+def personenDaten():
     inp = input("Sind Sie eine natürliche Person ja oder nein: ")
+    Personenlist = []
+    Firmalist = []
+    inp2 = "ja"
     if(inp == "ja"):
-        print(inp)
+        while(inp2 == "ja"):
+            name = input("Was ist dein Name? ")
+            geb = input("Hallo " + name + " wann hast du Geburtstag? ")
+            alter = input("Wie alt bist du " + name + "? ")
+            Personenlist.append([name,geb, alter])
+            print("Die Person: " + str(Personenlist) + " ist abgespeichert")
+            inp2 = input("Wollen Sie noch mehr Personen speichern? ")
     else:
-        print(inp)
+        inp22 = "ja"
+        while(inp22 == "ja"):
+            firmenname = input("Wie heißt deine Firma? ")
+            firmentyp = input("Was macht deine Firma? ")
+            gruendungsdatum = input("Wann wurde deine Firma gegründet? ")
+            steuernummer = input("Wie lautet die Steuernummer? ")
+            Firmalist.append([firmenname,firmentyp,gruendungsdatum,steuernummer])
+            print("Die Firma: " + str(Firmalist) + " ist abgespeichert")
+            inp22 = input("Wollen Sie noch mehr Firmen speichern? ")
+
+    return Personenlist, Firmalist
+
+
+def sortieren(personenListe, firmenListe):
+    inp10 = input("Möchten Sie die Liste Alphabetisch sortieren: ")
+    if(inp10 == "1"):
+        print(sorted(personenListe))
+    
+    
+
+def suchen():
+    print("suchen")
+
+def main():
+    plist = []
+    flist = []
+ 
+    a = "1"
+    while(a != "x"):
+        a = input("Was möchten Sie tun Datenspeichern(1), Sortieren(2), Suchen(3), Exit(x) ")
+        if(a=="1"):
+            plist, flist = personenDaten()
+        elif(a == "2"):
+            sortieren(plist, flist)
+        elif(a == "3"):
+            suchen()
+
+if __name__ == "__main__":
+    main()
+    
     
